324f9f4629c6bd9608391e8199ab2540f3b9ec08
Phil Looby <philip@silverfoxit.co.uk>
2013-03-23 23:28:17 +0000

    added stylesheet

--- config/application.rb
+++ config/application.rb
@@ -61,8 +61,8 @@ module PjlsampleApp
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
-    # Enable the asset pipeline
-    config.assets.enabled = true
+    # Enable the asset pipeline (disable for RoR tutorial)
+    config.assets.enabled = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ PjlsampleApp::Application.routes.draw do
 
   get "pages/contact"
   
+	get "pages/help"
+  
   get "pages/about"
 
   # The priority is based upon order of creation:
